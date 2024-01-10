778a444e13833e2584067eea363336dc1da227de
xsqu1zn't <fluffyspiderstudios@gmail.com>
2024-01-09 21:30:17 -1000

    prefix

--- modules/mongo/guildManager.js
+++ modules/mongo/guildManager.js
@@ -5,21 +5,46 @@ const models = {
 	guild: require("../../models/guildModel").model
 };
 
+/** @param {string} guild_id */
 async function exists(guild_id) {
 	return (await models.guild.exists({ _id: guild_id })) ? true : false;
 }
 
-async function fetch(guild_id, query = {}) {
-	return (await models.guild.findById(guild_id).lean()) || null;
+/** @param {string} guild_id @param {{}} query @param {boolean} upsert */
+async function fetch(guild_id, query = {}, upsert = false) {
+	if (!(await exists(guild_id)) && upsert) return await insert(guild_id);
+
+	return (await models.guild.findById(guild_id, query).lean()) || null;
 }
 
-async function insertNew(guild_id) {
+/** @param {string} guild_id @param {{}} query */
+async function update(guild_id, query) {
+	return await models.guild.findByIdAndUpdate(guild_id, query);
+}
+
+/** @param {string} guild_id */
+async function insert(guild_id) {
 	if (await exists(guild_id)) return;
 
 	let doc = new models.guild({ _id: guild_id });
 	return await doc.save();
 }
 
-async function fetchPrefix(guild_id) {}
+/** @param {string} guild_id */
+async function fetchPrefix(guild_id) {
+	return (await fetch(guild_id, { prefix: 1 }, true)).prefix || null;
+}
 
-async function setPrefix(guild_id, prefix) {}
+/** @param {string} guild_id @param {string} prefix */
+async function setPrefix(guild_id, prefix) {
+	await update(guild_id, { prefix });
+}
+
+module.exports = {
+	exists,
+	fetch,
+	update,
+	insert,
+	fetchPrefix,
+	setPrefix
+};
--- prefix_commands/prefix_CMD.js
+++ prefix_commands/prefix_CMD.js
@@ -1,6 +1,9 @@
 const { Client, Message } = require("discord.js");
 const { BetterEmbed } = require("../modules/discordTools");
+const { guildManager } = require("../modules/mongo");
 const jt = require("../modules/jsTools");
+
+const config = { client: require("../configs/config_client.json") };
 
 /** @type {import("../configs/typedefs").PrefixCommandExports} */
 module.exports = {
@@ -13,6 +16,24 @@ module.exports = {
 
 	/** @param {Client} client @param {Message} message @param {import("../configs/typedefs").PrefixCommandExtra} extra */
 	execute: async (client, message, { cleanContent }) => {
-        let args = cleanContent;
+		if (!cleanContent) {
+			// Fetch the current prefix for the guild
+			let prefix = (await guildManager.fetchPrefix(message.guild.id)) || config.client.PREFIX;
+
+			// Let the user know the result
+			return await message.reply({
+				content: `My prefix is \`${prefix}\`!`,
+				allowedMentions: { repliedUsers: false }
+			});
+		}
+
+		// Set the guild's prefix
+		await guildManager.setPrefix(message.guild.id, cleanContent);
+
+		// Let the user know the result
+		return await message.reply({
+			content: `Prefix changed to \`${cleanContent}\`.`,
+			allowedMentions: { repliedUsers: false }
+		});
 	}
 };
