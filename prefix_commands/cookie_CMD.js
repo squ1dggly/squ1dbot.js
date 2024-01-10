f5a10cffd2de75753b6420dbb821eb2dd0cc8c24
xsqu1zn't <fluffyspiderstudios@gmail.com>
2024-01-09 20:31:08 -1000

    yep

--- prefix_commands/cookie_CMD.js
+++ prefix_commands/cookie_CMD.js
@@ -6,17 +6,15 @@ const jt = require("../modules/jsTools");
 /** @type {import("../configs/typedefs").PrefixCommandExports} */
 module.exports = {
 	name: "cookie",
-	aliases: [],
 	description: "Get a cookie or a glass of milk",
 	category: "Fun",
-	options: { icon: "🍪", botAdminOnly: false, guildAdminOnly: false },
+	options: { icon: "🍪" },
 
 	/** @param {Client} client @param {Message} message @param {import("../configs/typedefs").PrefixCommandExtra} extra */
-	execute: async (client, message, { cleanContent, cmdName, prefix }) => {
-		// prettier-ignore
+	execute: async (client, message) => {
 		let choices = [
 			"What's up, **$USERNAME**! Have a cookie! :cookie:",
-			"Hey, **$USERNAME**! Have a glass of milk! :milk:",
+			"Hey, **$USERNAME**! Have a glass of milk! :milk:"
 		];
 
 		// prettier-ignore
