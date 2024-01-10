b5fdbe72850c55c713806a3848cd4226a9df0773
xsqu1zn't <fluffyspiderstudios@gmail.com>
2024-01-09 18:27:13 -1000

    oh yeah?

--- prefix_commands/help_CMD.js
+++ prefix_commands/help_CMD.js
@@ -6,6 +6,7 @@
 const { Client, Message } = require("discord.js");
 
 const { BetterEmbed } = require("../modules/discordTools");
+const jt = require("../modules/jsTools");
 
 module.exports = {
 	name: "help",
@@ -15,6 +16,16 @@ module.exports = {
 	execute: async (client, message, { prefix }) => {
 		// Get the current prefix commands and filter out ones that are set to be hidden
 		let commands = [...client.prefixCommands.values()].filter(cmd => !cmd?.options?.hidden);
+
+		// Check if there's available commands
+		if (!commands.length)
+			return await new BetterEmbed({ title: "❌ There aren't any commands available." }).reply(message);
+
+		// Get the available categories
+		let command_categories = jt.unique(
+			commands.map(cmd => ({ name: cmd.category, icon: cmd.categoryIcon || null })),
+			"name"
+		);
 
 		// Parse prefix commands into a readable format
 		let commands_f = [];
@@ -46,8 +57,43 @@ module.exports = {
 			if (_extra.length) _f += `\n${_extra.join("\n")}`;
 
 			// Push the formatted command to the main array
-			commands_f.push({ str: _f, category: cmd?.options?.category || null });
+			commands_f.push({ str: _f, name: cmd.name, category: cmd.category || null });
 		}
+
+		// Create an array to store each group of embeds for each command category
+		let embeds_categories = [];
+
+		// Iterate through the command categories and create the appropriate command pages
+		for (let category of command_categories) {
+			// Get all the commands for the current category
+			let _cmds = commands_f.filter(cmd => cmd.category === category.name);
+
+			// Make it a max of 10 command per page
+			let _cmds_split = jt.chunk(_cmds, 10);
+
+			// Create an array to store each "page" for the current category
+			let _embeds = [];
+
+			// Create an embed for each page
+			for (let i = 0; i < _cmds_split.length; i++) {
+				let group = _cmds_split[i];
+
+				// Create the embed :: { COMMANDS (PAGE) }
+				let _embed = new BetterEmbed({
+					title: "Help",
+					description: group.join("\n"),
+					footer: `Page ${i + 1} of ${_cmds_split.length}`
+				});
+
+				// Push the embed to the array
+				_embeds.push(_embed);
+			}
+
+			// Push the embed array to the main command category array
+			if (_embeds.length) embeds_categories.push(_embeds);
+		}
+
+		// Setup page navigation
 
 		// Create the embed :: { HELP }
 		let embed_help = new BetterEmbed({
