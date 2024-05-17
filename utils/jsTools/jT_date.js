fbd98b14a4a12cbc45e055644487f1187a52df48
xsqu1zn't <fluffyspiderstudios@gmail.com>
2024-05-16 23:12:47 -1000

    bug fix

--- utils/jsTools/jT_date.js
+++ utils/jsTools/jT_date.js
@@ -125,8 +125,8 @@ function eta(unix, options) {
  * @param {etaHMS_options} options
  *
  * @example
- * eta(1703001733955) // returns "1 hour, 0 minutes, and 0 seconds" (from now)
- * eta(1702994533936, { nullIfPast: true }) // returns null */
+ * etaHMS(1703001733955) // returns "1 hour, 0 minutes, and 0 seconds" (from now)
+ * etaHMS(1702994533936, { nullIfPast: true }) // returns null */
 function etaHMS(unix, options) {
 	unix = Number(unix);
 	if (isNaN(unix)) throw new Error("unix must be a number or string");
